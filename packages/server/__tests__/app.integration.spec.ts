8d8b7036d5531718d8d614cf0adf9b3ac703b2ba
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2023-04-04 19:00:39 +0100

    Extend test suite

--- packages/server/__tests__/app.integration.spec.ts
+++ packages/server/__tests__/app.integration.spec.ts
@@ -3,7 +3,13 @@ import request from "supertest";
 import app from "../src/app";
 
 describe("base application", () => {
-	it("exposes a /healthz endpoint", async () => {
-		await request(app).get("/healthz").expect(200, "OK");
+	describe("GET /health", () => {
+		it("returns 200 OK", async () => {
+			await request(app).get("/healthz").expect(200, "OK");
+		});
+
+		it("rejects other request methods", async () => {
+			await request(app).post("/healthz").expect(405, "Method Not Allowed");
+		});
 	});
 });
