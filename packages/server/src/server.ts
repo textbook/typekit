ea9d9d59b3f130349a51c6a8b1674db60dfc38ac
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2023-04-04 18:54:30 +0100

    Configure and apply linting

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -2,6 +2,6 @@ import app from "./app";
 
 const port = parseInt(process.env.PORT ?? "3000", 10);
 
-const server = app.listen(port, () => {
+app.listen(port, () => {
 	console.log(`listening on ${port}`);
 });
--- packages/server/src/utils/middleware.ts
+++ packages/server/src/utils/middleware.ts
@@ -2,4 +2,5 @@ import { RequestHandler } from "express";
 
 import { HttpStatus } from "./http";
 
-export const methodNotAllowed = (): RequestHandler => (_, res) => res.sendStatus(HttpStatus.METHOD_NOT_ALLOWED);
+export const methodNotAllowed = (): RequestHandler => (_, res) =>
+	res.sendStatus(HttpStatus.METHOD_NOT_ALLOWED);
