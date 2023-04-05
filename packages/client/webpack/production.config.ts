a96bc12202100a38447a76a0b2d480996c2680ba
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2023-04-05 10:18:05 +0100

    Externalise React(-DOM) to CDN

--- packages/client/webpack/production.config.ts
+++ packages/client/webpack/production.config.ts
@@ -1,6 +1,9 @@
 import { resolve } from "node:path";
 
+import HtmlWebpackTagsPlugin from "html-webpack-tags-plugin";
 import merge from "webpack-merge";
+
+import { dependencies } from "../package.json";
 
 import common from "./common.config";
 
@@ -11,4 +14,21 @@ export default merge(common, {
 		filename: "[name].[contenthash].js",
 		path: resolve(__dirname, "..", "..", "..", "dist", "static"),
 	},
+	plugins: [
+		new HtmlWebpackTagsPlugin({
+			scripts: (
+				[
+					{ packageName: "react", variableName: "React" },
+					{ packageName: "react-dom", variableName: "ReactDOM" },
+				] as const
+			).map(({ packageName, variableName }) => ({
+				attributes: { crossorigin: "" },
+				external: { packageName, variableName },
+				path: `https://cdnjs.cloudflare.com/ajax/libs/${packageName}/${dependencies[
+					packageName
+				].replace(/^[^~]/, "")}/umd/${packageName}.production.min.js`,
+			})),
+			usePublicPath: false,
+		}),
+	],
 });
